a091c8db79550c7829d5a530a7dc9ad2b4041c4f
Abel Arismendy <a.arismendy@uniandes.edu.co>
2022-08-28 08:25:04 -0500

    login and read

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import unittest
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from tkinter import messagebox
+
 
 
 class LoginUniandes(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path='./driver/chromedriver')
-        self.driver.get('https://www.uniandes.edu.co/')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get('https://login.ezproxy.uniandes.edu.co/login?qurl=http://www.ebooks7-24.com%2f%3fil%3d9168')
         self.driver.maximize_window()
         self.driver.implicitly_wait(10)
 
     def test_login(self):
-        self.driver.find_element_by_id('login').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.centrado1 > a').click()
+        # manual login
+        messagebox.showinfo('LOGIN', 'Please complete the login form and the 2-step verification process then click OK')
+
+        # read button
+        self.driver.find_element(By.XPATH, '//*[@id="button-1056-btnInnerEl"]').click()
+
+        messagebox.showinfo('READ', 'Please click OK to read the book')
 
 
 
